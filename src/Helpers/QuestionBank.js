3d0e38424626de1148c4e189fac0c5ecc77e8fb6
Chandan516593 <chandansingh15022003@gmail.com>
2024-07-23 18:54:16 +0530

    final commit

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,35 @@
+export const Question = [
+    {
+        prompt: "What is the capital of india ?",
+        optionA: "New Delhi",
+        optionB: "Mumbai",
+        optionC: "Jaipur",
+        optionD: "Pune",
+        answer : "A",
+    },
+    {
+        prompt: "Which is a Streaming Platform ?",
+        optionA: "Twitter",
+        optionB: "Twitch",
+        optionC: "Google",
+        optionD: "Robinhood",
+        answer : "B", 
+    },
+    {
+        prompt: "Army Institute of Technology loacted in?",
+        optionA: "Chennai",
+        optionB: "Hyderabad",
+        optionC: "Mumbai",
+        optionD: "Pune",
+        answer : "D", 
+    },
+    {
+        prompt: "Kya chandan chamar h?",
+        optionA: "Haan",
+        optionB: "Haan",
+        optionC: "Haan",
+        optionD: "Haan",
+        answer : "D", 
+
+    }
+];
